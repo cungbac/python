568ae722b0112adc1ae25d08a7eab73c52ed68af
unknown <cungbac.tran@gmail.com>
2021-04-11 23:37:23 +0700

    update K means

--- 2.DungLaiLapTrinh/3.AI/1.K-Means/K_means_pic_process.py
+++ 2.DungLaiLapTrinh/3.AI/1.K-Means/K_means_pic_process.py
@@ -1,8 +1,39 @@
 # import packages
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-import numpy
+import numpy as np
+import copy
 
 # read image
 img = plt.imread('pic1.jpg')
-print(img)
+
+height = img.shape[0]
+width = img.shape[1]
+
+# reshape image to 2D list
+img = img.reshape(height*width,3)
+
+img2 = np.zeros_like(img)
+
+# print(img.shape)
+
+# K means
+kmeans = KMeans(n_clusters=4).fit(img) # find clusters' coordinates
+labels = kmeans.predict(img)
+clusters = kmeans.cluster_centers_
+
+# update img
+# for i in range(len(clusters)):
+#     for j in range(len(img2)):
+#         if labels[j] == i:
+#             img2[j] = clusters[i]
+
+for i in range(len(img2)):
+    img2[i] = clusters[labels[i]]
+
+# Restore origin shape
+img2 = img2.reshape(height, width, 3)
+
+# Show image
+plt.imshow(img2)
+plt.show
